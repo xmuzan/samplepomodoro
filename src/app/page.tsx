0ac90e95ee78d5f3063df011ca44a63bfa02e143
Bil Game <bilgame36@gmail.com>
2025-07-15 23:23:26 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <FuturisticBorder>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col bg-background/80 backdrop-blur-sm text-foreground md:flex-row">
       <Navbar />
-      <main className="flex-1 p-4 pb-24 md:ml-20 md:pb-4 lg:ml-64">
+      <main className="flex flex-1 items-center justify-center p-4 pb-24 md:ml-20 md:pb-4 lg:ml-64">
         <TaskManager />
       </main>
     </div>
