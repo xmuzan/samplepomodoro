133d8d423080392a6d1b49f2f059b07f9416e616
Bil Game <bilgame36@gmail.com>
2025-07-18 15:18:52 +0000

    ai önerisi butonuna basıtğımsda ai suggestion failed ai önerisiyle ilgil

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI({apiKey: process.env.GOOGLE_API_KEY})],
+  plugins: [googleAI({apiKey: 'AIzaSyDeJgbB8s07f_qpc6aiwc8IN9QWXDlVzzE'})],
   model: 'googleai/gemini-2.0-flash',
 });
